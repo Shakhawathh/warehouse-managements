4f5523e4d19e6b2a18a4a6b8ad32ea4a537de451
Shakhawathh <shakhawathh55@gmail.com>
2022-05-09 23:35:46 +0600

    find

--- src/page/Blog/Blog.js
+++ src/page/Blog/Blog.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Blog = () => {
     return (
         <div className='text-center'>
-            <h3>Difference between javascript and nodej ?</h3>
+            <h3>Difference between javascript and nodejs ?</h3>
             <p>Both of these are used for programming. But there is a significant difference between Node.JS and Javascript. JavaScript (abbreviated as JS) is a lightweight scripting language that is object-oriented. It is used for developing HTML pages that are dynamic and have various interactive effects on their web pages. The Node.JS, on the other hand, helps in representing a list of all the methods and objects that the JavaScript code can access when run via node interpreters or in the V8 engine.</p>
         <br/>
-        <h3> Differences between sql and nosql databases ?</h3>
+        <h3> Differences between sql and nosql database ?</h3>
         <p>SQL databases are older so they have a more established fixed schema design and structure. NoSQL databases are easily scalable, flexible and relatively simple to use due to their flexible schema. SQL is the approach used to interact with relational databases, while NoSQL is used to interact with non-relational databases.</p>
         
         <br/>
